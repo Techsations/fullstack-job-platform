30903d626df2a8393f0d819cf9ba41f05ef873ef
Techsations <akandedamilare.t@gmail.com>
2023-07-20 12:24:41 +0100

    Started a fullstack job platform

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+
+const app = express()
+require("dotenv").config()
+app.use(bodyParser.json({limit:"100mb"}))
+app.use(bodyParser.urlencoded({ extended: true, limit: "100mb"}))
+app.use(cors({ origin: "*" }))
+
+
+
+const uri = process.env.MONGO_URI
+
+
+
+const connect = async () => {
+    try {
+        mongoose.set("strictQuery", false)
+        await mongoose.connect(uri)
+        console.log("MongoDB connected")
+    } catch (error) {
+        console.log(error)
+    }
+}
+connect()
+
+app.listen("5003", () => {
+    console.log("Server started")
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState} from 'react'
+import axios from 'axios'
+
+
+const App = ()=>  {
+  const [userName, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [data2, setData2] = useState([])
+
+
+
+
+  return (
+    <>
+    <div className='mx-auto container row'>
+        <div className='col-sm-8 shadow-lg p-5 mx-auto'>
+          <h6 className='text-muted display-6 text-center'>Sign UP</h6>
+          <input placeholder='User name' type="text" className=" my-2 form-control w-100" onChange={(e) => { setUserName(e.target.value) }} />
+          <input placeholder='Email' type="text" className=" my-2 form-control w-100" onChange={(e) => { setEmail(e.target.value) }} />
+          <input placeholder='Password' type="text" className=" my-2 form-control w-100" onChange={(e) => { setPassword(e.target.value) }} />
+          <button className=' my-2 btn btn-dark' onClick={register}>Submit</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
