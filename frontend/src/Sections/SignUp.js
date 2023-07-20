72f6cbc2400a748e0d33fe2747ee960cb71bdaa4
Techsations <akandedamilare.t@gmail.com>
2023-07-20 15:32:36 +0100

    created a post request from the frontend

--- frontend/src/Sections/SignUp.js
+++ frontend/src/Sections/SignUp.js
@@ -7,5 +7,29 @@ import { useNavigate } from "react-router-dom";
 
 
 const SignUp = () =>{
-    
+    // Post request
+
+    const navigate = useNavigate()
+  const onSubmit = (values) => {
+    axios.post("http://localhost:1010/create", values).then((res)=>{
+        console.log(res);
+        navigate('/login')
+    }).catch((err)=>{
+        console.log(err);
+    })
+  };
+
+    return (
+         <>
+            <div className='mx-auto container row'>
+                <div className='col-sm-8 shadow-lg p-5 mx-auto'>
+                    <h6 className='text-muted display-6 text-center'>Sign UP</h6>
+                    <input placeholder='User name' type="text" className=" my-2 form-control w-100" onChange={(e) => { setUserName(e.target.value) }} />
+                    <input placeholder='Email' type="text" className=" my-2 form-control w-100" onChange={(e) => { setEmail(e.target.value) }} />
+                    <input placeholder='Password' type="text" className=" my-2 form-control w-100" onChange={(e) => { setPassword(e.target.value) }} />
+                    <button className=' my-2 btn btn-dark' onClick={register}>Submit</button>
+                </div>
+            </div>
+        </>
+    )
 }
