d537bdc3d16fe4a04c0cc91d9200ba058d03ac13
Techsations <akandedamilare.t@gmail.com>
2023-07-20 12:43:07 +0100

    added the register user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,22 @@
 const userModel = require("../models/user.model")
 const bcryptjs = require("bcryptjs")
 const { generateToken, verifyToken } = require("../services/sessions")
+
+
+const register = async (req, res, next) => {
+    try {
+        let { userName, email, password } = req.body
+        const newUser = new userModel({
+            userName,
+            email,
+            password
+        })
+
+        const result = await newUser.save()
+        console.log(result)
+        sendMessage(email)
+        return res.status(201).send({ message: "Registration Successful", status: true })
+    } catch (error) {
+        next(error)
+    }
+}
