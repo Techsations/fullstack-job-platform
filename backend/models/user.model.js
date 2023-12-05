3ec9da49901e41e3251fc432aacc8d970f2f8197
Akande Timilehin <akandedamilare.t@gmail.com>
2023-12-05 14:44:25 +0100

    started connecting usermodel to database

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -36,18 +36,59 @@ const userSchema = new mongoose.Schema({
     skills: { type: [] }
 })
 
+// Employer Password encrypt
 let saltRound = 10
+employerSchema.pre("save", function (next) {
+    if (this.password != undefined) {
+        bcryptjs.hash(this.password, saltRound).then((hashed) => {
+            this.password = hashed
+            next()
+        }).catch((error) => {
+            console.log(error, 33);
+        })
+    }
+})
+
+
+// User password encryption
 userSchema.pre("save", function (next) {
     if(this .password != undefined) {
         bcryptjs.hash(this.password, saltRound).then((hashed)=>{
             this.password = hashed
             next()
         }).catch((error)=>{
-            console.log(error);
+            console.log(error, 33);
         })
     }
 })
 
+
+// Employer job posting schema
+const postedJobsSchema = new mongoose.Schema({
+    jobTitle: { type: String },
+    email: { type: String },
+    jobDescription: { type: String },
+    timestamp: { type: String },
+    salaryType: { type: String },
+    min_salary: { type: Number, trim: true },
+    max_salary: { type: Number, trim: true },
+    jobType: { type: String },
+    requiredSkills: { type: [] },
+    author: { type: String }
+})
+
+// Job Application Schema
+const jobApplicationsSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String },
+    userEmail: { type: String, unique: true },
+    cv_url: { type: String },
+    timestamp: { type: String },
+    jobID: {type: String}
+})
+
+
+const employerModel = mongoose.models.employer_tbs || mongoose.model("employer_tbs", employerSchema)
 
 const userModel = mongoose.models.user_tbs || mongoose.model("user_tbs", userSchema)
 
