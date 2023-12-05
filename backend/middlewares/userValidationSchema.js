0d0dbd300258aaf06016676dd317eed1297225b3
Akande Timilehin <akandedamilare.t@gmail.com>
2023-12-05 15:29:13 +0100

    completed error handling in the middlewares folder

--- backend/middlewares/userValidationSchema.js
+++ backend/middlewares/userValidationSchema.js
@@ -17,3 +17,23 @@ const userValidationSchema = yup.object().shape({
                 .required("Password is required")
                 .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/, "Password should contain at least an Upper case, number and a special character"),
 })
+
+const employerValidationSchema = yup.object().shape({
+    employerName: yup
+                .string("Username must be a string")
+                .min(2, "Username is too short")
+                .max(50, "Username is too long")
+                .required("Username is required")
+                .matches(/^[a-zA-Z0-9]+$/, "Employer Name should not contain special characters"),
+    email: yup
+            .string("Email must be a string")
+            .email("Email is required"),
+    password: yup
+                .string("Password must be a string")
+                .min(8, "Password is too short")
+                .max(50, "Password is too short")
+                .required("Password is required")
+                .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/, "Password should contain at least an Upper case, number and a special character"),
+})
+
+module.exports = {userValidationSchema, employerValidationSchema}
