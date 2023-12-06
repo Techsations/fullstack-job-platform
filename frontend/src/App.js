c3d87468dde2a224bc983911bb033d222f3e71cf
Akande Timilehin <akandedamilare.t@gmail.com>
2023-12-06 14:12:15 +0100

    configured app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./Sections/SignIn";
-import SignUp from "./SignUp";
-import { useNavigate } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './SignUp';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import SelectSkill from './JobSeeker/SelectSkill';
+import UserDashboard from './JobSeeker/UserDashboard';
+import UserLogin from './JobSeeker/UserLogin';
+import EmployerDashboard from './Employer/EmployerDashboard';
+import EmployerLogin from './Employer/EmployerLogin';
+import PostJob from './Employer/PostJob';
+import ApplyForJob from './JobSeeker/ApplyForJob';
+import ManageJobs from './Employer/ManageJobs';
+import EditPostedJob from './Employer/EditPostedJob';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/SignIn" element={<SignIn />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignUp />} />
+        <Route path='/home' element={<Navigate to={"/"} />} />
+        <Route path='/user/skills' element={<SelectSkill />} />
+        <Route path='/userLogin' element={<UserLogin />} />
+        <Route path='/employerLogin' element={<EmployerLogin />} />
+        <Route path='/jobSeekerDashboard' element={<UserDashboard />} />
+        <Route path='/employerDashboard' element={<EmployerDashboard />} />
+        <Route path='/postJob' element={<PostJob />} />
+        <Route path='/applyJob' element={<ApplyForJob />} />
+        
+
+        <Route path='/manage_jobs'>
+          <Route index element={<ManageJobs />}></Route>
+          <Route path='edit_job' element={<EditPostedJob />} />
+        </Route>
+
+      </Routes>
     </>
   );
 }
