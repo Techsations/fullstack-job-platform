52589c474ddec9a056909076c42f566f750c63f0
Akande Timilehin <akandedamilare.t@gmail.com>
2023-12-06 14:23:19 +0100

    added skills api

---
+++ frontend/src/SkillsApi.js
@@ -0,0 +1,18 @@
+
+const programmingSkills = [
+    { id: 1, name: 'HTML' },
+    { id: 2, name: 'CSS' },
+    { id: 3, name: 'JavaScript' },
+    { id: 4, name: 'Python' },
+    { id: 5, name: 'Java' },
+    { id: 6, name: 'C++' },
+    { id: 7, name: 'C#' },
+    { id: 8, name: 'React.js' },
+    { id: 9, name: 'Node.js' },
+    { id: 10, name: 'PHP' },
+    { id: 11, name: 'Vue.js' },
+    { id: 12, name: 'GIT' },
+    { id: 13, name: 'MySQL' },
+    { id: 14, name: 'UI/UX' },
+    { id: 15, name: 'Data Science' },
+]
