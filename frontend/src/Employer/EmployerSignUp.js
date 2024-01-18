7be9927244d763e75f1a7032f36a7ae6a2e7ce7c
Akande Timilehin <akandedamilare.t@gmail.com>
2024-01-18 20:40:53 +0100

    Employer signup page

---
+++ frontend/src/Employer/EmployerSignUp.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import SpinnerLoader from '../SpinnerLoader'
+import { router } from '../Router/Router'
+
+function EmployerSignUp({ registerAsJobSeeker, loginAsEmployer, setisLoading, isSpinning }) {
+    const navigate = useNavigate()
+    const onSubmit = (values, errors) => {
+        // setTimeout(() => {
+        //     setisLoading(false)
+        // }, 1990);
+        setTimeout(() => {
+            if (values.password !== values.confirmPassword) {
+                alert("Password does not matched")
+                return
+            } else {
+                setisLoading(true)
+                const uri = `${router}/users/registerAsEmployer`
+                axios.post(uri, values).then((res) => {
+                    console.log(res);
+                    alert(res.data.message)
+                    setisLoading(false)
+                    navigate("/employerLogin")
+                }).catch((err) => {
+                    console.log(err);
+                    alert(err.response.data.message)
+                    setisLoading(false)
+                })
+            }
+        }, 2000);
+    }
+
+    const { handleSubmit, handleChange, errors, touched, handleBlur, values } = useFormik({
+        initialValues: {
+            employerName: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        },
+        validationSchema: yup.object().shape({
+            employerName: yup.string()
+                .min(2, "Employer name is too short")
+                .max(50, "Employer name is too long")
+                .required("Employer name cannot be empty")
+                .matches(/^[a-zA-Z0-9]+$/, "Employer Name should not contain special characters"),
+            email: yup.string()
+                .email()
+                .required("This input cannot be empty"),
+            password: yup.string().required("This input cannot be empty")
+                .min(8, "should be at least 8 characters")
+                .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/, "Password should contain at least an Upper case, number and a special character"),
+            confirmPassword: yup.string().required("This input cannot be empty")
+                .min(8, "should be at least 8 characters")
+        }),
+        onSubmit
+    })
+
+    const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
+    const handleCheckboxChange = (event) => {
+        setIsCheckboxChecked(event.target.checked);
+    };
+
+
+    return (
+        <>
+            <div className='position-relative'>
+                <form action="" onSubmit={handleSubmit}>
+                    <h5 className='register-text text-center py-2'>Register as an Employer</h5>
+                    <div className='my-3'>
+                        <input type="text" onBlur={handleBlur} value={values.employerName} onChange={handleChange} placeholder='Employer Name' name="employerName" className='form-control' />
+                        {touched.employerName && errors.employerName &&
+                            <small className='text-danger fw-bold'>{errors.employerName}</small>
+                        }
+                    </div>
+                    <div className='my-3'>
+                        <input type="email" onBlur={handleBlur} value={values.email} onChange={handleChange} placeholder='Employer Email' name="email" className='form-control' />
+                        {touched.email && errors.email &&
+                            <small className='text-danger fw-bold'>{errors.email}</small>
+                        }
+                    </div>
+                    <div className='my-3'>
+                        <input type="password" onBlur={handleBlur} value={values.password} onChange={handleChange} placeholder='Password' name="password" className='form-control' />
+                        {touched.password && errors.password &&
+                            <small className='text-danger fw-bold'>{errors.password}</small>
+                        }
+                    </div>
+                    <div className='my-3'>
+                        <input type="password" onBlur={handleBlur} value={values.confirmPassword} onChange={handleChange} placeholder='Confirm Password' name="confirmPassword" className='form-control' />
+                        {touched.confirmPassword && errors.confirmPassword &&
+                            <small className='text-danger fw-bold'>{errors.confirmPassword}</small>
+                        }
+                    </div>
+                    <div className='d-flex'>
+                        <div>
+                            <input onChange={handleCheckboxChange} className='check-box' type="checkbox" name="" id="" />
+                        </div>
+                        <p className='ms-2 terms'>I agree with JobNet's Terms of Service, Privacy Policy, and default Notification Settings.</p>
+                    </div>
+                    <div className='text-center'>
+                        <button disabled={!isCheckboxChecked} type="submit" className='btn btn-dark'>Create Account</button>
+                    </div>
+                    <div className='d-flex mt-2'>
+                        <small className='mx-auto'>To register as a Job Seeker, <span onClick={registerAsJobSeeker} className='text-primary job-seeker'>Click here</span></small>
+                    </div>
+                    <div className='d-flex mt-2'>
+                        <small className='mx-auto already-have-an-account'>Already have an Account? <span onClick={loginAsEmployer} className='text-primary job-seeker'>Login here</span></small>
+                    </div>
+                </form>
+                <div className='position-absolute small-loader w-100'>
+                    {isSpinning ? <SpinnerLoader /> : null}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EmployerSignUp
